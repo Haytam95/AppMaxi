3abbbd7a57f5488788ca61a84d8fe3aa4f4c00a7
Maxi <maxifiorito77@gmail.com>
2021-11-03 14:23:32 -0300

    fixes

--- src/app/carga/carga.component.ts
+++ src/app/carga/carga.component.ts
@@ -1,19 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { DataListService } from '../data-list.service';
 import { Router } from '@angular/router';
+import * as Quill from 'quill';
 
 @Component({
   selector: 'app-carga',
   templateUrl: './carga.component.html',
   styleUrls: ['./carga.component.css']
 })
-export class CargaComponent implements OnInit {
+export class CargaComponent implements OnInit, AfterViewInit {
 
   public titulo: string;
   public content: string;
   public selectedLanguaje;
   public langlist;
+  public toolbarOptions = [['bold', 'italic'], ['link', 'image']];
 
   @Input() public data;
   @Input() public exist;
@@ -22,6 +24,15 @@ export class CargaComponent implements OnInit {
   ngOnInit(): void {
     this.datalist.getDataLang().subscribe((resolve) => {
       this.langlist = resolve;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    const quill = new Quill('#editor', {
+      modules: {
+        toolbar: '#toolbar'
+      },
+      theme: 'snow'
     });
   }
 
@@ -46,9 +57,12 @@ export class CargaComponent implements OnInit {
   }
 
   public onBasicUpload(event): void {
-
-    
   }
+
+  public testPage(): void {
+    console.log('Funciona!');
+  }
+
   public reloadPage(): void {
     const currentUrl = this.router.url;
     this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
